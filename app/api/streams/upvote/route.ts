1873efc02240a25b534b8738d277ad0139e5cb49
AbhilakshayAnand <aa8518@srmist.edu.in>
2024-12-25 23:30:50 +0530

    25th

---
+++ app/api/streams/upvote/route.ts
@@ -0,0 +1,42 @@
+import { prismaClient } from "@/app/lib/db";
+import { getServerSession } from "next-auth";
+import { NextRequest } from "next/server";
+import { z } from "zod";
+
+const UpvoteSchema = z.object({
+    streamId: z.string(),
+})
+
+export async function POST(req: NextRequest) {
+    const session = await getServerSession();
+
+    if (!session?.user?.email) {
+        return new Response("Unauthorized", { status: 403 });
+    }
+    
+    const user = await prismaClient.user.findFirst({
+        where: {
+            email: session?.user?.email ?? ""
+        }
+    });
+    if(!user) {
+        return new Response("Unauthorized", { status: 403 });
+    }
+
+    try {
+        const data = UpvoteSchema.parse(await req.json());
+
+        await prismaClient.upvote.create({
+            data: {
+                userId: user.id,
+                streamId: data.streamId
+            }
+        })
+
+    } catch (error) {
+        return new Response("Error while upvoting stream", { status: 403 });
+        
+    }
+
+    const data = UpvoteSchema.parse(await req.json());
+}
