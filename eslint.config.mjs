ad7674874925832681c20d8c1889cdc15c5e67cb
AbhilakshayAnand <aa8518@srmist.edu.in>
2025-02-20 21:12:30 +0530

    logs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,14 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off", // Ignore unused variables
+      "@typescript-eslint/ban-ts-comment": "off", // Allow @ts-ignore
+      "@typescript-eslint/no-explicit-any": "off", // Allow 'any' type
+      "react-hooks/exhaustive-deps": "warn", // Warn instead of error for missing deps
+    },
+  },
 ];
 
 export default eslintConfig;
